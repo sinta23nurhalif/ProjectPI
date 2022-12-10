84c1085eca5abb083733c76949aa1f7a7ba1d880
SINTA NURHALIFAH <92354983+sinta23nurhalif@users.noreply.github.com>
2022-12-10 22:51:11 +0700

    Add files via upload

---
+++ project/inputdata.py
@@ -0,0 +1,23 @@
+from Tampilan import Tampilan
+import Bukti
+import datetime
+
+def inputdata(para):
+    #ambulance_rate = ambulance_rate.rent_ambulance()
+    print("Tanggal pemesanan (dd/mm/yyyy): ")
+    day = int(input("Hari: "))
+    month = int(input("Bulan: "))
+    year = int(input("Tahun: "))
+    booking_date = datetime.date(year, month, day)
+    Tampilan.date = str(booking_date)
+
+    # Dapatkan input lama sewa
+    duration = int(input("Lama sewa: "))
+    nama_penyewa = input("Nama penyewa: ")
+    Tampilan.bayar = para*duration
+    Tampilan.a = str(Tampilan.bayar)
+    def printdata():
+        print('\t   =====        DATA PENYEWAAN      =====')
+        return  nama_penyewa, booking_date, duration, Tampilan.bayar
+    Bukti.Bukti(printdata(), 60, Tampilan.a, Tampilan.date)
+    
