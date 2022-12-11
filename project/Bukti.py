90c862e6a96646e466fd665343a45848b6b1dda5
SINTA NURHALIFAH <92354983+sinta23nurhalif@users.noreply.github.com>
2022-12-11 21:34:37 +0700

    Update Bukti.py

--- project/Bukti.py
+++ project/Bukti.py
@@ -1,6 +1,10 @@
+from Tampilan import Tampilan
 import Bill
 import inputdata
-from Tampilan import Tampilan
+import os
+import Mulai
+import AmbulanceTersedia
+
 
 def Bukti(c,w,a, date):
 #w fungsinya untuk parameter jumlah karakter yang ingin ditampilkan
@@ -11,4 +15,21 @@ def Bukti(c,w,a, date):
   print("Apakah data anda sudah benar? (Y/T)")
   pilihan = input("Masukkan Input : ")
   if (pilihan == 'Y' or pilihan == 'y'):
-    Bill.Bill(Tampilan.x, Tampilan.a, Tampilan.date)
+    Bill.Bill(Tampilan.mobil, Tampilan.total, Tampilan.date)
+    os.system('clear')
+    print("Transaksi Selesai")
+    print("\nIngin kembali ke menu? (Y/T)")
+    ingput = input("Masukkan input : ").upper()
+    if (ingput == 'Y'):
+      Mulai.Mulai()
+    elif (ingput == 'T'):
+      print("\nTerima Kasih!")
+      exit()
+  elif (pilihan == 'T' or pilihan == 't'):
+    AmbulanceTersedia.AmbulanceTersedia()
+  else:
+    print("Masukkan inputan yang valid!")
+    input()
+    Mulai.Mulai()
+    
+    
